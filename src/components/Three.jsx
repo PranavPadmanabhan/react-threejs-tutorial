a20a2937c435d47aaa63cc9fce026e8821239b1e
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2022-08-24 13:45:48 +0530

    second commit

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -1,53 +1,35 @@
 import { OrbitControls } from '@react-three/drei'
-import { useFrame, useLoader, useThree } from '@react-three/fiber'
-import * as THREE from 'three'
+import { useFrame, useLoader } from '@react-three/fiber'
 import React, { useRef } from 'react'
 import { TextureLoader } from 'three'
 
 const Three = () => {
 
-  const texture1 = useLoader(TextureLoader, 'photo.jpg')
-  const texture2 = useLoader(TextureLoader, 'PRANAV.jpg')
-
+  const texture = useLoader(TextureLoader, 'photo.jpg')
   const cameraControlRef = useRef()
   const planeRef = useRef();
 
-  const materials = [
-    new THREE.MeshStandardMaterial({map:texture1}),
-    new THREE.MeshStandardMaterial({map:texture2}),
-    new THREE.MeshStandardMaterial({map:texture1}),
-    new THREE.MeshStandardMaterial({map:texture2}),
-    new THREE.MeshStandardMaterial({map:texture1}),
-    new THREE.MeshStandardMaterial({map:texture2}),
-  ]
-
-  const geometry = new THREE.BoxBufferGeometry(2,2,2)
-
-  const { scene } = useThree();
-  const mesh = new THREE.Mesh(geometry,materials)
-  
   useFrame((state) => {
     if(window.innerWidth>600){
-        mesh.rotation.y = -state.mouse.x * 4;
-        mesh.rotation.z = -state.mouse.y;
-       
+      if(planeRef.current){
+        planeRef.current.rotation.y = -state.mouse.x * 4;
+        planeRef.current.rotation.z = -state.mouse.y;
+       }
     }
   })
-
-  scene.add(mesh)
 
 
 
 
   return (
     <>
-      <ambientLight args={['grey', 1]}/>
+      <ambientLight args={['#ffffff', 1]}/>
       <perspectiveCamera args={[75, window.innerWidth/window.innerHeight,0.1,1000]}/>
       <OrbitControls ref={cameraControlRef} enableDamping/>
-      {/* <mesh ref={planeRef}>
+      <mesh ref={planeRef}>
         <boxBufferGeometry args={[2,2,2,32,32,32]}/>
-        <meshBasicMaterial  map={texture}/>
-      </mesh> */}
+        <meshBasicMaterial map={texture}/>
+      </mesh>
     </>
   )
 }
